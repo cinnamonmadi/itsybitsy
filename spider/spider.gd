68131fe1df59f210ccb1314c98ec1e89912a3246
cinnamonmadi <cinnamon.madi@gmail.com>
2021-12-05 15:55:17 -0600

    Beginning of webslinging
    Signed-off-by: cinnamonmadi <cinnamon.madi@gmail.com>

--- spider/spider.gd
+++ spider/spider.gd
@@ -13,7 +13,8 @@ const MAX_FALL_SPEED = 150
 const JUMP_IMPULSE = 200
 const JUMP_INPUT_DURATION = 0.06
 const COYOTE_TIME_DURATION = 0.06
-const WEB_RADIUS = 300
+const WEB_RADIUS = 150
+const TILE_SIZE = 32
 
 var direction = 0
 var grounded = false
@@ -78,40 +79,49 @@ func update_sprite():
 		sprite.flip_h = not sprite.flip_h
 
 func update_web():
-	if web_position != null and position.distance_to(web_position) > ((WEB_SEARCH_RADIUS + 1) * 32):
-		web_position = null
+	# if web_position != null and position.distance_to(web_position) > WEB_RADIUS:
+		# web_position = null
 	if web_position == null:
 		web.set_point_position(1, Vector2.ZERO)
 	else:
 		web.set_point_position(1, web_position - position)
 
+func tile_distance(tile_position):
+	return pow(tile_position.x - position.x, 2) + pow(tile_position.y - position.y, 2) - pow(WEB_RADIUS, 2)
+
 func nearest_web_tile():
-	var player_coordinates = tilemap.world_to_map(position)
-	var nearest_point = null
+	var nearest_tile = null
+	var nearest_tile_dist = -1
 
-	for search_radius in range(0, WEB_SEARCH_RADIUS + 1):
-		print(search_radius)
-		var start = player_coordinates - Vector2(search_radius, search_radius)
-		var end = player_coordinates + Vector2(search_radius, search_radius)
-		var search_points = []
-
+	var start = Vector2(floor((position.x - WEB_RADIUS) / TILE_SIZE), floor((position.y - WEB_RADIUS) / TILE_SIZE))
+	var end = Vector2(floor((position.x + WEB_RADIUS) / TILE_SIZE), floor((position.y + WEB_RADIUS) / TILE_SIZE))
+	for y in range(start.y, end.y + 1):
 		for x in range(start.x, end.x + 1):
-			search_points.append(Vector2(x, start.y))
-			search_points.append(Vector2(x, end.y))
-		for y in range(start.y + 1, end.y):
-			search_points.append(Vector2(start.x, y))
-			search_points.append(Vector2(end.x, y))
+			var tile_point = Vector2(x, y)
+			var tile_position = tile_point * TILE_SIZE
 
-		for point in search_points:
-			if tilemap.get_cellv(point) == 1:
-				return tilemap.map_to_world(point)
-	return null
+			var dist_top_left = position.distance_to(tile_position + Vector2(TILE_SIZE, 0))
+			var dist_top_right = position.distance_to(tile_position + Vector2(TILE_SIZE, 0))
+			var dist_bottom_left = position.distance_to(tile_position + Vector2(0, TILE_SIZE))
+			var dist_bottom_right = position.distance_to(tile_position + Vector2(TILE_SIZE, TILE_SIZE))
+
+			var dist_min = min(dist_top_left, min(dist_top_right, min(dist_bottom_left, dist_bottom_right)))
+
+			if dist_min > WEB_RADIUS or tilemap.get_cellv(tile_point) != 1:
+				continue
+
+			var tile_center = tile_position + Vector2(TILE_SIZE / 2.0, TILE_SIZE / 2.0)
+			web_raycast.cast_to = tile_center - position
+			web_raycast.force_raycast_update()
+			var raycast_point = web_raycast.get_collision_point()
+			if tile_center.distance_to(raycast_point) > sqrt(2 * pow(TILE_SIZE / 2.0, 2)):
+				continue
+
+			if nearest_tile == null or position.distance_to(raycast_point) < nearest_tile_dist:
+				nearest_tile = raycast_point
+				nearest_tile_dist = position.distance_to(raycast_point)
+
+	return nearest_tile
 
 func websling():
-	web_position = null
-	var nearest_tile = nearest_web_tile()
-	if nearest_tile != null:
-		web_raycast.cast_to = (nearest_tile - position) + Vector2(16, 16)
-		web_raycast.force_raycast_update()
-		var collision = web_raycast.get_collider()
-		if collision == tilemap:
+	web_position = nearest_web_tile()
